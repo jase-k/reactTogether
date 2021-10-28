9b383ed990e76fcf3461da102c42e9e039aebbc3
Jase Kraft <jase@perfectmypodcast.com>
2021-10-28 18:38:04 -0500

    added state

--- src/PersonCard.js
+++ src/PersonCard.js
@@ -1,10 +1,17 @@
+import { useState } from "react"
+
 function PersonCard(props){
-    let {firstName, lastName, age, hairColor} = props
+    let {firstName, lastName, age: propsAge, hairColor} = props
+    const [age, setAge] = useState(propsAge)
+    function handleClick(){
+        setAge(age + 1)
+    }
     return(
         <div>
             <h1>{lastName}, {firstName}</h1>
             <p>Age: {age}</p>
             <p>Hair Color: {hairColor}</p>
+            <button onClick={handleClick}>Birthday Button</button>
         </div>
     )
 }
